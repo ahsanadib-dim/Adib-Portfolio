8e296580ffcaf8f2f11a027a96a26c7aecfd42df
Ahasan Adib <ahasanadib2005@gmail.com>
2026-05-15 00:24:02 +0600

    Update main.tsx

--- src/lib/main.tsx
+++ src/lib/main.tsx
@@ -1,7 +1,7 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '../App.tsx'; // এখানে একটি বাড়তি ডট (.) যোগ করা হয়েছে
+import '../index.css'; // এখানেও একটি বাড়তি ডট (.) যোগ করা হয়েছে
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
